cfd26ee133abbbf8613dc3472046b615fb628dcc
ngcb03 <ngcamargob@gmail.com>
2023-06-08 21:36:19 -0500

    ¡Revisado y aprobado!

---
+++ PROYECTOS/src/AdministradorTareas/modelo/Tareas.java
@@ -0,0 +1,54 @@
+package AdministradorTareas.modelo;
+
+public class Tareas {
+
+    private int id_tarea;
+    private int id_incremental = 0;
+    private String descripción;
+    private String fecha_vencimiento;
+    private String estado;
+
+    public Tareas(){
+
+    }
+
+    public Tareas(String descripción, String fecha_vencimiento, String estado){
+        this.id_tarea = id_incremental;
+        id_incremental+=1;
+        this.descripción = descripción;
+        this.fecha_vencimiento = fecha_vencimiento;
+        this.estado = estado;
+    }
+
+    public int getId_tarea() {
+        return id_tarea;
+    }
+
+    public void setId_tarea(int id_tarea) {
+        this.id_tarea = id_tarea;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+
+    public String getFecha_vencimiento() {
+        return fecha_vencimiento;
+    }
+
+    public void setFecha_vencimiento(String fecha_vencimiento) {
+        this.fecha_vencimiento = fecha_vencimiento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
---
+++ PROYECTOS/src/AdministradorTareas/vista/Main.java
@@ -0,0 +1,20 @@
+package AdministradorTareas.vista;
+
+/*
+* Autor: Nicolas Camargo
+*
+* Administrador de tareas: Desarrolla una aplicación que permita a los
+* usuarios crear, editar y eliminar tareas. Debe permitir asignar una
+* descripción, fecha de vencimiento y estado (completado o pendiente)
+* a cada tarea.
+*
+* Última modificación: 08/06/2023
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
